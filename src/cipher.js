fece4c21305f856b1ee3cbb52f18194c263c4957
nirvanarockk@hotmail.com <nirvanarockk@hotmail.com>
2019-02-05 13:18:42 -0600

    C

--- src/cipher.js
+++ src/cipher.js
@@ -3,21 +3,25 @@ window.cipher = {
 input = input.toUpperCase();
     let output = "" ;
     for (let i = 0; i < input.length; i++) {
-      let letter = input.charCodeAt(i);
-
-      let newLetter =((letter - 65 + key) % 26) + 65;
-      output = output +(String.fromCharCode(newLetter));
-
-  }
-      return output;
+      if(input.charCodeAt(i) === 32){
+        output = output + (String.fromCharCode(32));
+      }else{
+        let letter = input.charCodeAt(i);
+        let newLetter =((letter - 65 + key) % 26) + 65;
+        output = output +(String.fromCharCode(newLetter));
+      }
+}
+  return output;
     },
 
     decode: (key,input) => {
       input = input.toUpperCase();
         let output = "" ;
-        for (let i = 0; i < input.length; i++) {
+        for (let i = 0; i < input.length; i++)
+        if (input.charCodeAt(i) === 32){
+          output = output + (String.fromCharCode(32));
+        }else{
           let letter = input.charCodeAt(i);
-
           let newLetter = ((letter + 65 - key) % 26) + 65;
           output = output +(String.fromCharCode(newLetter));
       }
